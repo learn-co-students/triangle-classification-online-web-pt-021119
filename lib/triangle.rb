94df72c0a93382b93d13435ec7bdec4a12344ce9
Henry Sztanski <henry@sztanski.com>
2019-03-23 02:15:47 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  attr_reader :s1, :s2, :s3
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    if self.s1 + self.s2 <= self.s3
+      raise TriangleError
+    elsif self.s1 == self.s2 && self.s2 == self.s3 #equilateral
+      :equilateral
+    elsif condition #scalene
+      :scalene
+    elsif condition #isosceles
+      :isosceles
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
