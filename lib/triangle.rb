f816cccff4f80638af08f6bb297f02d902cffe6b
Henry Sztanski <henry@sztanski.com>
2019-03-23 02:32:24 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,18 +8,23 @@ class Triangle
   end
 
   def kind
-    if self.s1 + self.s2 <= self.s3
+    if (@s1 + @s2 <= @s3) ||
+       (@s2 + @s3 <= @s1) ||
+       (@s1 + @s3 <= @s2) ||
+       @s1 <= 0 || @s2 <= 0 || @s3 <= 0
       raise TriangleError
-    elsif self.s1 == self.s2 && self.s2 == self.s3 #equilateral
+    elsif @s1 == @s2 && @s2 == @s3 #equilateral
       :equilateral
-    elsif self.s1 != self.s2 && self.s2 != self.s3 && self.s3 != self.s1 #scalene
+    elsif @s1 != @s2 && @s2 != @s3 && @s3 != @s1 #scalene
       :scalene
-    elsif self.s1 == self.s2 || self.s2 == self.s3 || self.s3 == self.s1 #isosceles
+    elsif @s1 == @s2 || @s2 == @s3 || @s3 == @s1 #isosceles
       :isosceles
     end
   end
 
   class TriangleError < StandardError
-    # triangle error code
+    def message
+      "This is not a triangle!"
+    end
   end
 end
