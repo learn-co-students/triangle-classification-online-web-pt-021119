769c0ab4c4264b86d44551d27ded352b21718c6c
Eyal Toledano <eyaltoledano@me.com>
2019-03-24 02:36:18 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,34 +9,40 @@ class Triangle
     @side3 = side3
   end
 
+  def tri_equality?
+    sorted_sides = [@side1, @side2, @side3].sort
+    greatest_side = sorted_sides.pop
+    greatest_side < sorted_sides.sum
+  end
+
   def valid?
     sides_array = [@side1, @side2, @side3]
     if sides_array.any? {|side| side == 0}
       @valid = false
     elsif sides_array.any? {|side| side < 0}
       @valid = false
-    # ADD ELSIF: if the sum of the lengths of any two sides of a triangle is smaller than the length of the third side, then it is
+    elsif self.tri_equality? == false
+      @valid = false
     elsif sides_array.any? {|side| side.is_a?(Float)}
-      binding.pry
       @valid = true
     else
       @valid = true
     end
+    @valid
   end
 
   def kind
     sides_array = [@side1, @side2, @side3]
     if self.valid? == false
-      # raise an error if the triangle is invalid
       raise TriangleError
     else
-      @type = :equilateral if @side1 == @side2 && @side1 == @side3
-      binding.pry
-      if
+      if ((@side1 == @side2) && (@side1 == @side3) && (@side2 == @side3))
+        @type = :equilateral
+      elsif ((@side1 == @side2) || (@side1 == @side3) || (@side2 == @side3))
         @type = :isosceles
+      else
+        @type = :scalene
       end
-        # :scalene
-          # no equal sides
     end
     @type
   end
