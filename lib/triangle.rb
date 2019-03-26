cddf96dd6ec89c8fcf3b48945c6e8bd946616541
Haj B <phajib@gmail.com>
2019-03-26 00:49:17 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   # end
 
   def kind
-    if (side1 <= 0 || side2 <=0 || side3 <=0) ||(side1+side2 <= side3) || (side1+side3 <= side2) || (side2+side3 <= side1)
+    if (side1 <= 0 || side2 <=0 || side3 <=0) || (side1+side2 <= side3 || side1+side3 <= side2 || side2+side3 <= side1)
       raise TriangleError
     elsif 
       side1 == side2 && side2 == side3
@@ -25,6 +25,6 @@ class Triangle
   end
 
   class TriangleError < StandardError
-    "Hello"
+    "One or more sides is 0 or less than 0, please check you sides length!"
   end
 end
